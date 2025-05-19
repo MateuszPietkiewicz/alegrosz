a0eefba1728f87af379903061ede7d7f501c4557
MateuszPietkiewicz <mateuszpietkiewicz1@gmail.com>
2025-05-19 21:37:30 +0200

    location model

---
+++ app/db/models/location.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.db.session import Base
+
+class Location(Base):
+    __tablename__ = "location"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    address = Column(String(200), nullable=False)
+    capacity = Column(Integer, nullable=False)
+
+
+    inventory_items = relationship("InventoryItem", back_populates="location", cascade="all, delete")
+
+    def __repr__(self):
+        return f"{type(self).__name__}(id={self.id}, name={self.name})"
